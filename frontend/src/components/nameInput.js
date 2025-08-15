6370c3598b16dedf2a500245b43336e61bd5384b
Daniel Zabielski <e12119018@student.tuwien.ac.at>
2025-08-15 16:10:28 +0200

    Full wipe

--- frontend/src/components/nameInput.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-
-export default function NameInput({value, setValue}) {
-
-    const handleChange = (event) => setValue(event.target.value);
-
-    return <input type="text" value={value} onChange={handleChange}></input>
-
-}
